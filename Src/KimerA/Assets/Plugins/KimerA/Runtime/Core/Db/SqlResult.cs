fd14b9ad1bb0ca0cc10260ad09ecfa6ea0ca80ec
HACBIT <hacbit@foxmail.com>
2025-05-05 16:41:37 +0800

    [fix] While not have odin plugin, SqlResult will not show in inspector instead of throw exception

--- Src/KimerA/Assets/Plugins/KimerA/Runtime/Core/Db/SqlResult.cs
+++ Src/KimerA/Assets/Plugins/KimerA/Runtime/Core/Db/SqlResult.cs
@@ -1,17 +1,25 @@
 using System;
 using System.Collections.Generic;
+#if ODIN_INSPECTOR
 using Sirenix.OdinInspector;
+#endif
 
 namespace KimerA.Db
 {
     [Serializable]
     public sealed class SqlResult
     {
+#if ODIN_INSPECTOR
         [ShowInInspector]
+#endif
         public bool Success { get; set; }
+#if ODIN_INSPECTOR
         [ShowInInspector]
+#endif
         public string Message { get; set; }
+#if ODIN_INSPECTOR
         [ShowInInspector]
+#endif
         public List<Dictionary<string, object>> Data { get; set; }
 
         public SqlResult()
