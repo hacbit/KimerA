6159d9754a07d93f4e65d3060a12b16f91690414
HACBIT <hacbit@foxmail.com>
2025-05-18 03:20:45 +0800

    [fix] 修复读档空字符串反序列化问题，优化生成器构建脚本

--- Src/build.py
+++ Src/build.py
@@ -8,8 +8,17 @@ import json
 
 BASE_DIR = os.path.dirname(__file__)
 
+class Project:
+    """
+    Project class to represent a project
+    """
+    def __init__(self, name: str, modify: float = 0.0):
+        self.name: str = name
+        self.modify: float = modify
+
 with open(os.path.join(BASE_DIR, "build.json"), encoding="utf8") as f:
-    ANALYZER_PROJECTS = json.load(f)["analyzers"]
+    data = json.load(f)["analyzers"]
+    ANALYZER_PROJECTS: list[Project] = [Project(proj["name"], proj["modify"]) for proj in data]
 
 TARGET_DIR = os.path.join(BASE_DIR, "KimerA", "Assets", "Plugins", "KimerA", "Runtime", "Analyzers")
 
@@ -32,27 +41,67 @@ def build_proj(proj_name: str) -> None:
     proj_path = get_proj_path(proj_name)
     os.system(f"dotnet build {proj_path} -c Release")
 
+def get_newest_modify_time(proj_path: str) -> float:
+    """
+    Get the newest modify time of the project.
+    Skip the bin/ obj/ folders
+    """
+    newest_time = 0
+    for root, _, files in os.walk(proj_path):
+        if "bin" in root or "obj" in root:
+            continue
+        for file in files:
+            file_path = os.path.join(root, file)
+            if os.path.getmtime(file_path) > newest_time:
+                newest_time = os.path.getmtime(file_path)
+    return newest_time
+
+def check_should_build(proj: Project) -> bool:
+    """
+    Check if the project is modified and should be built
+    """
+    proj_path = get_proj_path(proj.name)
+    if not os.path.exists(proj_path):
+        return False
+    # Check if the project file is modified
+    newest_time = get_newest_modify_time(os.path.dirname(proj_path))
+    if newest_time > proj.modify:
+        proj.modify = newest_time
+        return True
+    return False
+
 def main():
     """
     Build all the analyzer projects,
     and copy the .dll files to the target directory
     """
-    for proj_name in ANALYZER_PROJECTS:
-        proj = get_proj_path(proj_name)
-        if not os.path.exists(proj):
-            print(f"Project {proj_name} not found")
+    for proj in ANALYZER_PROJECTS:
+        proj_path = get_proj_path(proj.name)
+        if not os.path.exists(proj_path):
+            print(f"==> Project {proj} not found")
             continue
-        build_proj(proj_name)
-        print(f"Project {proj_name} built successfully")
-        dll_path = get_release_dll_path(proj_name)
+        if not check_should_build(proj):
+            print(f"==> Project {proj.name} not modified, skipping build")
+            continue
+        build_proj(proj.name)
+        print(f"==> Project {proj.name} built successfully")
+        # Update build.json
+        with open(os.path.join(BASE_DIR, "build.json"), "w", encoding="utf8") as f:
+            json.dump(
+                {"analyzers": [{"name": p.name, "modify": p.modify} for p in ANALYZER_PROJECTS]},
+                f,
+                indent=4,
+                ensure_ascii=False
+            )
+        dll_path = get_release_dll_path(proj.name)
         if not os.path.exists(dll_path):
-            print(f"Project {proj_name} .dll not found")
+            print(f"==> Project {proj.name} .dll not found")
             continue
         if not os.path.exists(TARGET_DIR):
             os.makedirs(TARGET_DIR)
-        target = os.path.join(TARGET_DIR, f"{proj_name}.dll")
+        target = os.path.join(TARGET_DIR, f"{proj.name}.dll")
         shutil.copy(dll_path, target)
-        print(f"{proj_name}.dll copied to {target}")
+        print(f"==> {proj.name}.dll copied to {target}")
 
 if __name__ == "__main__":
     main()
